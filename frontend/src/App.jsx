f9c923e87bdf91ae69c8f4d13faa880a2ea3999c
Ashiq121914 <ashiq121914@gmail.com>
2024-09-21 20:47:57 +0600

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
 
 const App = () => {
   return (
-    <div>
+    <div className='app'>
+      <Navbar/>
       
     </div>
   )
