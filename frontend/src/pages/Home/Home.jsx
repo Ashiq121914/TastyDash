faf7f1a1dbccb86b5d516a8de5d65d53bee25583
Ashiq121914 <ashiq121914@gmail.com>
2024-09-30 18:50:51 +0600

    header, explore menu

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import "./Home.css"
+import React, { useState } from "react";
+import "./Home.css";
+import Header from "../../components/Header/Header.jsx";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
+
   return (
     <div>
-      
+      <Header></Header>
+      <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
