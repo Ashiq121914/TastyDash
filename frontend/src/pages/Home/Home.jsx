c6a7c9f4888e4ebdd97c7a77c094a29b6426e3a0
Ashiq121914 <ashiq121914@gmail.com>
2024-10-02 15:46:50 +0600

    food display, Store context, cart managing state, filter food from explore menu

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header.jsx";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay.jsx";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -11,6 +12,7 @@ const Home = () => {
     <div>
       <Header></Header>
       <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
+      <FoodDisplay category={category}></FoodDisplay>
     </div>
   );
 };
