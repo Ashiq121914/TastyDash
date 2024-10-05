2dea33aeb88761b5ddbc938153d4aa9f225b4f70
Ashiq121914 <ashiq121914@gmail.com>
2024-10-05 20:45:42 +0600

    Making responsive of all the components, smooth scrolling

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,55 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "./Navbar.css";
-import { assets } from '../../assets/assets';
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    // for the underline effect for menu items
-    const [menu,setMenu] = useState("home")
+  // for the underline effect for menu items
+  const [menu, setMenu] = useState("home");
   return (
-    <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
-        <ul className="navbar-menu">
-            <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
-            <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-            <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-            <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
-        </ul>
-      <div className="navbar-right"> 
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </a>
+      </ul>
+      <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
-            
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
         </div>
         <button>sign in</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
