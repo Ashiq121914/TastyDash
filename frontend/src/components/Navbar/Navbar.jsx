9c4a1e02d2c8c2dc3ff23ddfc735f8f3703c1134
Ashiq121914 <ashiq121914@gmail.com>
2024-10-09 15:10:58 +0600

    Create Sign in / Sign up Component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   // for the underline effect for menu items
   const [menu, setMenu] = useState("home");
   return (
@@ -46,7 +46,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
   );
