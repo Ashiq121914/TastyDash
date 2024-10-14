235f5141a7552329c4a110960195d48d2c7d2f5f
Ashiq121914 <ashiq121914@gmail.com>
2024-10-14 14:30:18 +0600

    place order feature and stripe payment integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Cart from "./pages/Cart/Cart.jsx";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import LoginPopup from "./components/LoginPopup/LoginPopup.jsx";
+import Verify from "./pages/Verify/Verify.jsx";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/" element={<Home />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
           <Route path="/order" element={<PlaceOrder />}></Route>
+          <Route path="/verify" element={<Verify />}></Route>
         </Routes>
       </div>
       <Footer></Footer>
