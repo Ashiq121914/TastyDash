4c611119406fe330805bc3594efe7c2d199db176
Ashiq121914 <ashiq121914@gmail.com>
2024-10-22 13:48:42 +0600

    final push

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://tasty-dash-backend.vercel.app";
   return (
     <div>
       <ToastContainer />
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,8 +6,8 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
-  // const url = "https://tasty-dash-backend.vercel.app";
-  const url = "https://localhost:4000";
+  const url = "https://tasty-dash-backend.vercel.app";
+  // const url = "https://localhost:4000";
 
   const [token, setToken] = useState("");
 
